10dc504097acfda27e7b3e65a215281f67dcf02b
Jeremy Ward <jeremy@3five.com>
2016-08-03 11:50:30 -0500

    Simplify functions to get rid of nasty else conditions. Add function get WP_User object.

--- 3five-cupp.php
+++ 3five-cupp.php
@@ -71,99 +71,93 @@ add_action( 'admin_enqueue_scripts', 'cupp_enqueue_scripts_styles' );
  * Show the new image field in the user profile page.
  *
  * @param $user
- *
- * @return bool
  */
 function cupp_profile_img_fields( $user ) {
 	if ( ! current_user_can( 'upload_files' ) ) {
-		return false;
+		return;
 	}
 
 	// vars
-	$cupp_url             = get_the_author_meta( 'cupp_meta', $user->ID );
-	$cupp_upload_url      = get_the_author_meta( 'cupp_upload_meta', $user->ID );
-	$cupp_upload_edit_url = get_the_author_meta( 'cupp_upload_edit_meta', $user->ID );
+	$url             = get_the_author_meta( 'cupp_meta', $user->ID );
+	$upload_url      = get_the_author_meta( 'cupp_upload_meta', $user->ID );
+	$upload_edit_url = get_the_author_meta( 'cupp_upload_edit_meta', $user->ID );
+	$button_text     = $upload_url ? 'Change Current Image' : 'Upload New Image';
 
-	if ( ! $cupp_upload_url ) {
-		$btn_text = 'Upload New Image';
-	} else {
-		$cupp_upload_edit_url = get_home_url() . get_the_author_meta( 'cupp_upload_edit_meta', $user->ID );
-		$btn_text             = 'Change Current Image';
+	if ( $upload_url ) {
+		$upload_edit_url = get_home_url() . $upload_edit_url;
 	}
 	?>
-    <div id="cupp_container">
-    <h3><?php _e( 'Custom User Profile Photo', 'custom-user-profile-photo' ); ?></h3>
 
-	    <table class="form-table">
-		    <tr>
-			    <th><label for="cupp_meta"><?php _e( 'Profile Photo', 'custom-user-profile-photo' ); ?></label></th>
-			    <td>
-				    <!-- Outputs the image after save -->
-				    <div id="current_img">
-					    <?php if ( $cupp_upload_url ): ?>
-						    <img src="<?php echo esc_url( $cupp_upload_url ); ?>" class="cupp-current-img">
-						    <div class="edit_options uploaded">
-							    <a class="remove_img"><span>Remove</span></a>
-							    <a href="<?php echo esc_url( $cupp_upload_edit_url ); ?>" class="edit_img" target="_blank"><span>Edit</span></a>
-						    </div>
-					    <?php elseif ( $cupp_url ) : ?>
-						    <img src="<?php echo esc_url( $cupp_url ); ?>" class="cupp-current-img">
-						    <div class="edit_options single">
-							    <a class="remove_img"><span>Remove</span></a>
-						    </div>
-					    <?php else : ?>
-						    <img src="<?php echo esc_url( plugins_url( 'custom-user-profile-photo/img/placeholder.gif' ) ); ?>"
-						         class="cupp-current-img placeholder">
-					    <?php endif; ?>
-				    </div>
+	<div id="cupp_container">
+		<h3><?php _e( 'Custom User Profile Photo', 'custom-user-profile-photo' ); ?></h3>
 
-				    <!-- Select an option: Upload to WPMU or External URL -->
-				    <div id="cupp_options">
-					    <input type="radio" id="upload_option" name="img_option" value="upload" class="tog" checked>
-					    <label for="upload_option">Upload New Image</label><br>
-					    <input type="radio" id="external_option" name="img_option" value="external" class="tog">
-					    <label for="external_option">Use External URL</label><br>
-				    </div>
+		<table class="form-table">
+			<tr>
+				<th><label for="cupp_meta"><?php _e( 'Profile Photo', 'custom-user-profile-photo' ); ?></label></th>
+				<td>
+					<!-- Outputs the image after save -->
+					<div id="current_img">
+						<?php if ( $upload_url ): ?>
+							<img src="<?php echo esc_url( $upload_url ); ?>" class="cupp-current-img">
+							<div class="edit_options uploaded">
+								<a class="remove_img"><span><?php _e( 'Remove', 'custom-user-profile-photo' ); ?></span></a>
+								<a href="<?php echo esc_url( $upload_edit_url ); ?>" class="edit_img"
+								   target="_blank"><span><?php _e( 'Edit', 'custom-user-profile-photo' ); ?></span></a>
+							</div>
+						<?php elseif ( $url ) : ?>
+							<img src="<?php echo esc_url( $url ); ?>" class="cupp-current-img">
+							<div class="edit_options single">
+								<a class="remove_img"><span><?php _e( 'Remove', 'custom-user-profile-photo' ); ?></span></a>
+							</div>
+						<?php else : ?>
+							<img
+								src="<?php echo esc_url( plugins_url( 'custom-user-profile-photo/img/placeholder.gif' ) ); ?>"
+								class="cupp-current-img placeholder">
+						<?php endif; ?>
+					</div>
 
-				    <!-- Hold the value here if this is a WPMU image -->
-				    <div id="cupp_upload">
-					    <input type="hidden" name="cupp_placeholder_meta" id="cupp_placeholder_meta"
-					           value="<?php echo esc_url( plugins_url( 'custom-user-profile-photo/img/placeholder.gif' ) ); ?>"
-					           class="hidden" />
-					    <input type="hidden" name="cupp_upload_meta" id="cupp_upload_meta"
-					           value="<?php echo esc_url_raw( $cupp_upload_url ); ?>" class="hidden" />
-					    <input type="hidden" name="cupp_upload_edit_meta" id="cupp_upload_edit_meta"
-					           value="<?php echo esc_url_raw( $cupp_upload_edit_url ); ?>" class="hidden" />
-					    <input type='button' class="cupp_wpmu_button button-primary"
-					           value="<?php _e( esc_attr( $btn_text ), 'custom-user-profile-photo' ); ?>" id="uploadimage" /><br />
-				    </div>
-				    <!-- Outputs the text field and displays the URL of the image retrieved by the media uploader -->
-				    <div id="cupp_external">
-					    <input type="text" name="cupp_meta" id="cupp_meta"
-					           value="<?php echo esc_url_raw( $cupp_url ); ?>" class="regular-text" />
-				    </div>
-				    <!-- Outputs the save button -->
-				    <span class="description">
-					    <?php
-					    _e(
-						    'Upload a custom photo for your user profile or use a URL to a pre-existing photo.',
-						    'custom-user-profile-photo'
-					    );
-					    ?>
-				    </span>
+					<!-- Select an option: Upload to WPMU or External URL -->
+					<div id="cupp_options">
+						<input type="radio" id="upload_option" name="img_option" value="upload" class="tog" checked>
+						<label for="upload_option"><?php _e( 'Upload New Image', 'custom-user-profile-photo' ); ?></label><br>
+						<input type="radio" id="external_option" name="img_option" value="external" class="tog">
+						<label for="external_option"><?php _e( 'Use External URL', 'custom-user-profile-photo' ); ?></label><br>
+					</div>
 
-				    <p class="description">
-					    <?php _e( 'Update Profile to save your changes.', 'custom-user-profile-photo' ); ?>
-				    </p>
-			    </td>
-		    </tr>
+					<!-- Hold the value here if this is a WPMU image -->
+					<div id="cupp_upload">
+						<input type="hidden" name="cupp_placeholder_meta" id="cupp_placeholder_meta"
+						       value="<?php echo esc_url( plugins_url( 'custom-user-profile-photo/img/placeholder.gif' ) ); ?>"
+						       class="hidden" />
+						<input type="hidden" name="cupp_upload_meta" id="cupp_upload_meta"
+						       value="<?php echo esc_url_raw( $upload_url ); ?>" class="hidden" />
+						<input type="hidden" name="cupp_upload_edit_meta" id="cupp_upload_edit_meta"
+						       value="<?php echo esc_url_raw( $upload_edit_url ); ?>" class="hidden" />
+						<input type='button' class="cupp_wpmu_button button-primary"
+						       value="<?php _e( esc_attr( $button_text ), 'custom-user-profile-photo' ); ?>"
+						       id="uploadimage" />
+						<br />
+					</div>
+					<!-- Outputs the text field and displays the URL of the image retrieved by the media uploader -->
+					<div id="cupp_external">
+						<input type="text" name="cupp_meta" id="cupp_meta"
+						       value="<?php echo esc_url_raw( $url ); ?>" class="regular-text" />
+					</div>
+					<!-- Outputs the save button -->
+					<span
+						class="description"><?php _e( 'Upload a custom photo for your user profile or use a URL to a pre-existing photo.', 'custom-user-profile-photo' ); ?></span>
+					<p class="description">
+						<?php _e( 'Update Profile to save your changes.', 'custom-user-profile-photo' ); ?>
+					</p>
+				</td>
+			</tr>
 
-	    </table><!-- end form-table -->
-    </div> <!-- end #cupp_container -->
+		</table><!-- end form-table -->
+	</div> <!-- end #cupp_container -->
 
-    <?php wp_enqueue_media(); // Enqueue the WordPress Media Uploader ?>
-
-<?php
+	<?php
+	// Enqueue the WordPress Media Uploader
+	wp_enqueue_media();
 }
 
 add_action( 'show_user_profile', 'cupp_profile_img_fields' );
@@ -173,22 +167,27 @@ add_action( 'edit_user_profile', 'cupp_profile_img_fields' );
 /**
  * Save the new user CUPP url.
  *
- * @param $user_id
- *
- * @return bool
+ * @param int $user_id
  */
 function cupp_save_img_meta( $user_id ) {
-
 	if ( ! current_user_can( 'upload_files', $user_id ) ) {
-		return false;
+		return;
 	}
 
-	// @codingStandardsIgnoreStart TODO: filter and sanitize $_POST data
-	// If the current user can edit Users, allow this.
-	update_user_meta( $user_id, 'cupp_meta', $_POST['cupp_meta'] );
-	update_user_meta( $user_id, 'cupp_upload_meta', $_POST['cupp_upload_meta'] );
-	update_user_meta( $user_id, 'cupp_upload_edit_meta', $_POST['cupp_upload_edit_meta'] );
-	// @codingStandardsIgnoreStop
+	$values = array(
+		// String value. Empty in this case.
+		'cupp_meta'             => filter_input( INPUT_POST, 'cupp_meta', FILTER_SANITIZE_STRING ),
+
+		// File path, e.g., http://3five.dev/wp-content/plugins/custom-user-profile-photo/img/placeholder.gif
+		'cupp_upload_meta' => filter_input( INPUT_POST, 'cupp_upload_meta', FILTER_SANITIZE_URL ),
+
+		// Edit path, e.g., /wp-admin/post.php?post=32&action=edit&image-editor,
+		'cupp_upload_edit_meta' => filter_input( INPUT_POST, 'cupp_upload_edit_meta', FILTER_SANITIZE_URL ),
+	);
+
+	foreach ( $values as $key => $value ) {
+		update_user_meta( $user_id, $key, $value ); // @codingStandardsIgnoreLine
+	}
 }
 
 add_action( 'personal_options_update', 'cupp_save_img_meta' );
@@ -197,58 +196,45 @@ add_action( 'edit_user_profile_update', 'cupp_save_img_meta' );
 /**
  * Retrieve the appropriate image size
  *
- * @param WP_User|Int $user_id Default: $post->post_author. Will accept any valid user ID passed into this parameter.
+ * @param int $user_id Default: $post->post_author. Will accept any valid user ID passed into this parameter.
  * @param string      $size    Default: 'thumbnail'. Accepts all default WordPress sizes and any custom sizes made by
  *                             the add_image_size() function.
  *
  * @return string      (Url) Use this inside the src attribute of an image tag or where you need to call the image url.
  */
-function get_cupp_meta( $user_id, $size ) {
-    global $post;
+function get_cupp_meta( $user_id, $size = 'thumbnail' ) {
+	global $post;
 
-    //allow the user to specify the image size
-    if (!$size){
-        $size = 'thumbnail'; // Default image size if not specified.
-    }
-    if(!$user_id || !is_numeric( $user_id ) ){
-        // Here we're assuming that the avatar being called is the author of the post. 
-        // The theory is that when a number is not supplied, this function is being used to 
-        // get the avatar of a post author using get_avatar() and an email address is supplied 
-        // for the $id_or_email parameter. We need an integer to get the custom image so we force that here.
-        // Also, many themes use get_avatar on the single post pages and pass it the author email address so this
-        // acts as a fall back.
-        $user_id = $post->post_author; 
-    }
-    
-    // get the custom uploaded image
-    $attachment_upload_url = esc_url( get_the_author_meta( 'cupp_upload_meta', $user_id ) );
-    
-    // get the external image
-    $attachment_ext_url = esc_url( get_the_author_meta( 'cupp_meta', $user_id ) );
-    $attachment_url = '';
-    $image_url = '';
-    if($attachment_upload_url){
-        $attachment_url = $attachment_upload_url;
-        
-        // grabs the id from the URL using the WordPress function attachment_url_to_postid @since 4.0.0
-        $attachment_id = attachment_url_to_postid( $attachment_url );
-     
-        // retrieve the thumbnail size of our image
-        $image_thumb = wp_get_attachment_image_src( $attachment_id, $size );
-        $image_url = $image_thumb[0];
-
-    } elseif($attachment_ext_url) {
-        $image_url = $attachment_ext_url;
-    }
-
-	if ( empty( $image_url ) ) {
-		return '';
+	if ( ! $user_id || ! is_numeric( $user_id ) ) {
+		/*
+		 * Here we're assuming that the avatar being called is the author of the post.
+		 * The theory is that when a number is not supplied, this function is being used to
+		 * get the avatar of a post author using get_avatar() and an email address is supplied
+		 * for the $id_or_email parameter. We need an integer to get the custom image so we force that here.
+		 * Also, many themes use get_avatar on the single post pages and pass it the author email address so this
+		 * acts as a fall back.
+		 */
+		$user_id = $post->post_author;
 	}
 
-    // return the image thumbnail
-    return $image_url;
-}
+	// Check first for a custom uploaded image
+	$attachment_upload_url = esc_url( get_the_author_meta( 'cupp_upload_meta', $user_id ) );
 
+	if ( $attachment_upload_url ) {
+		// grabs the id from the URL using the WordPress function attachment_url_to_postid @since 4.0.0
+		$attachment_id = attachment_url_to_postid( $attachment_upload_url );
+
+		// Retrieve the thumbnail size of our image. Should return an array with first index value containing the URL.
+		$image_thumb = wp_get_attachment_image_src( $attachment_id, $size );
+
+		return isset( $image_thumb[0] ) ? $image_thumb[0] : '';
+	}
+
+	// Finally, check for image from an external URL. If none exists, return an empty string.
+	$attachment_ext_url = esc_url( get_the_author_meta( 'cupp_meta', $user_id ) );
+
+	return $attachment_ext_url ? $attachment_ext_url : '';
+}
 
 
 /**
@@ -257,44 +243,39 @@ function get_cupp_meta( $user_id, $size ) {
  * Replaces the WordPress avatar with your custom photo using the get_avatar hook.
  *
  * @param $avatar
- * @param $id_or_email
+ * @param int|object|string $identifier
  * @param $size
  * @param $alt
  *
  * @return string
  */
-function cupp_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
-    $user = false;
-    $id = false;
+function cupp_avatar( $avatar, $identifier, $size, $alt ) {
+	if ( $user = cupp_get_user_by_id_or_email( $identifier ) ) {
+		if ( $custom_avatar = get_cupp_meta( $user->ID, 'thumbnail' ) ) {
+			return "<img alt='{$alt}' src='{$custom_avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+		}
+	}
 
-    if ( is_numeric( $id_or_email ) ) {
-
-        $id = (int) $id_or_email;
-        $user = get_user_by( 'id' , $id );
-
-    } elseif ( is_object( $id_or_email ) ) {
-
-        if ( ! empty( $id_or_email->user_id ) ) {
-            $id = (int) $id_or_email->user_id;
-            $user = get_user_by( 'id' , $id );
-        }
-
-    } else {
-        // $id = (int) $id_or_email;
-        $user = get_user_by( 'email', $id_or_email );   
-    }
-
-    if ( $user && is_object( $user ) ) {
-
-        $custom_avatar = get_cupp_meta($id, 'thumbnail');
-
-        if (isset($custom_avatar) && !empty($custom_avatar)) {
-            $avatar = "<img alt='{$alt}' src='{$custom_avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
-        }
-
-    }
-
-    return $avatar;
+	return $avatar;
 }
 
-add_filter( 'get_avatar', 'cupp_avatar' , 1 , 5 );
+add_filter( 'get_avatar', 'cupp_avatar', 1, 5 );
+
+/**
+ * Get a WordPress User by ID or email
+ *
+ * @param $identifer
+ *
+ * @return false|WP_User
+ */
+function cupp_get_user_by_id_or_email( $identifer ) {
+	if ( is_numeric( $identifer ) ) {
+		return get_user_by( 'id', (int) $identifer );
+	}
+
+	if ( property_exists( $identifer, 'user_id' ) ) {
+		return get_user_by( 'id', (int) $identifer->user_id );
+	}
+
+	return get_user_by( 'email', $identifer );
+}
